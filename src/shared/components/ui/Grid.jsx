b8022c70ef6c2efd74098c2d196afdeeb66a8f7d
yoacinsoy <brnarea@protonmail.com>
2026-03-17 18:17:49 -0300

    refactor: remove draggable grid functionality from Grid component

--- src/shared/components/ui/Grid.jsx
+++ src/shared/components/ui/Grid.jsx
@@ -1,24 +1,13 @@
 "use client";
 
-import { useRef } from "react";
-import { useDraggableGrid } from "@/shared/hooks/useDraggableGrid";
-
 export default function Grid({ children }) {
-  const gridRef = useRef(null);
-  const { position, onMouseDown, onMouseMove, onMouseUp } = useDraggableGrid();
 
-  return (
-    <div
-      ref={gridRef}
-      onMouseDown={onMouseDown}
-      onMouseMove={onMouseMove}
-      onMouseUp={onMouseUp}
-      onMouseLeave={onMouseUp}
-      aria-hidden="true"
-      className="fixed inset-0 grid-bg z-30"
-      style={{ backgroundPosition: `${position.x}px ${position.y}px` }}
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div
+            aria-hidden="true"
+            className="fixed inset-0 grid-bg z-30"
+        >
+            {children}
+        </div>
+    );
 }
