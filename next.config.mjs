0b661ba27feb4bdc295fd0f536177ed1b03e57e5
yoacinsoy <brnarea@protonmail.com>
2026-03-17 17:48:55 -0300

    refactor: reorganize next.config.mjs to structure image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+            }
+        ],
+    },
+    reactCompiler: true,
 };
 
 export default nextConfig;
